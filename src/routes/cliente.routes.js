e84b7a1c6496a8d6058d56a32f5b4d3a0f320fbb
Mikelopez27 <aglmiguel4@gmail.com>
2024-03-07 01:11:49 -0600

    VP3.2.4.1.1

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -9,7 +9,7 @@ router.get("/selectCliente", middleware, cliente.select)
 router.post("/busquedaXcel", middleware, cliente.busqedaxcel)
 router.post("/selectXempcli", middleware, cliente.selectXemp)
 router.post("/EmpCli", middleware, cliente.empcli)
-router.post("/ContTipCli",cliente.conttipocli)
+router.post("/ContTipCli", middleware, cliente.conttipocli)
 router.post("/agregarCliente", middleware, cliente.agregar)
 router.put("/editarCliente/:cli_clave", middleware, cliente.editar)
 router.delete("/eliminarCliente/:cli_clave", middleware, cliente.eliminar);
